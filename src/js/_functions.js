bf58c091982a6c7783c6b3cc64a4a534892029a3
Elena Gluzdova <elenagavrilova003@gmail.com>
2024-05-09 10:46:32 +0300

    first commit

---
+++ src/js/_functions.js
@@ -0,0 +1,8 @@
+// Реализация бургер-меню
+import { burger } from './functions/burger';
+
+// Реализация остановки скролла (не забудьте вызвать функцию)
+import { disableScroll } from './functions/disable-scroll';
+
+// Реализация включения скролла (не забудьте вызвать функцию)
+import { enableScroll } from './functions/enable-scroll';
